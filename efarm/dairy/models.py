7e89a870d9e7418d9ad5fbaeb84052f7c359dfdc
Peter Evance <peterevance1@gmail.com>
2023-02-24 23:46:43 +0300

    Models refactor

--- efarm/dairy/models.py
+++ efarm/dairy/models.py
@@ -92,110 +92,6 @@ class Cow(models.Model):
     def __str__(self):
         return self.name
     
-    
-class Milk(models.Model):
-    """Stores information about milk production in our farm"""
-    class Meta:
-        verbose_name_plural = "Milk"
-        unique_together = ('cow', 'milking_time_value')
-    MILKING_TIMES = ((1, "Morning"),(2, "Mid-Morning"),(3, "Evening"), (4, "Night"))
-    milking_time_value = models.IntegerField(editable=False, choices=MILKING_TIMES)
-    milking_date = models.DateTimeField(auto_now_add=True)
-    cow = models.ForeignKey(Cow, on_delete=models.CASCADE, related_name='milk')
-    amount_in_kgs = models.DecimalField(verbose_name="Amount (kg)", default=0.00 ,max_digits=5,decimal_places=2, blank=True, null=True, 
-                                        validators=[MinValueValidator(0, message="Amount of milk can not be less than 0 Kgs."), MaxValueValidator(35, message="Amount of milk cannot be more than 35.0 Kgs.")])
-       
-    @property
-    def milking_time(self):
-        return self.get_milking_time_value_display()
-
-    def set_milking_time(self):
-        hour = self.milking_date.hour
-        if 3 <= hour < 8:
-            self.milking_time_value = 1
-        elif 8 <= hour < 13:
-            self.milking_time_value = 2
-        elif 13 <= hour < 18:
-            self.milking_time_value = 3
-        else:
-            self.milking_time_value = 4
-         
-    def __str__(self):
-        return f"Milk record of cow {self.cow.name} at {self.milking_time} on {self.milking_date.strftime('%Y-%m-%d %H:%M:%S')}"
-
-    def clean(self):
-        cow = self.cow
-        cow_age = cow.get_cow_age()
-        if cow.availability_status == "D":
-            raise ValidationError("Cannot add milk record for a dead cow.")
-        if cow.availability_status == "S":
-            raise ValidationError("Cannot add milk record for sold cow.")
-        if cow_age < 21*30:  # 21 months old
-            raise ValidationError('Cow is less than 21 months old and should not have a milk record')
-
-        if self.milking_date is None:
-            self.milking_date = timezone.now()
-        try:
-            existing_milk_record = Milk.objects.get(cow=self.cow, milking_date__date=self.milking_date.date(), milking_time_value=self.milking_time_value)
-            if existing_milk_record and not self.pk:
-                raise ValidationError("A milk record already exists for this cow at this milking time.")
-        except Milk.DoesNotExist:
-            pass
-
-        if cow.gender != "F":
-            raise ValidationError("This cow is not female and cannot produce milk.")
-        
-        self.set_milking_time()
-        if not self.milking_date:
-            self.milking_date = timezone.now()
-        if self.amount_in_kgs <= 0:
-            raise ValidationError("Amount in kgs should be greater than 0")
-
-
-class Heat(models.Model):
-    """
-    The Heat model represents a record of a cow being in heat.
-    """
-    class Meta:
-        verbose_name_plural = "Heat records"
-    observation_time = models.DateTimeField()
-    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)   
-    
-    def __str__(self):
-        return f"Heat record for cow {self.cow.tag_number} on {self.observation_time}"
-    
-    def clean(self):
-        # Check 1: Ensure that observation time is not in the future
-        if self.observation_time > timezone.now():
-            raise ValidationError('Observation time cannot be in the future.')
-        
-        # Check 2: Ensure that cow is not already pregnant
-        if self.cow.pregnancy_status == 'P':
-            raise ValidationError('Cow is already pregnant.')
-        
-        # Check 3: Ensure that cow is not already in heat
-        if self.cow.heat_set.filter(observation_time__range=(timezone.now() - timedelta(days=2), timezone.now() + timedelta(days=2))).exists():
-            raise ValidationError('Cow is already in heat.')
-        
-        # Check 4: Ensure that cow is alive
-        if self.cow.availability_status == 'D':
-            raise ValidationError('Cow is dead and cannot be in heat.')
-        # Check 5: Ensure that cow is female
-        if self.cow.gender == 'M':
-            raise ValidationError('Heat can only be observed in female cows.')
-        
-        # Check 6: Ensure that cow is not calved in the last 60 days
-        if self.cow.pregnancy_status == 'C' and self.observation_time - self.cow.pregnancy.calving_date < timedelta(days=60):
-            raise ValidationError('Cow cannot be in heat within 60 days after calving.')
-        
-        # Check 7: Ensure that cow is not in heat within 21 days
-        if self.cow.heat_set.filter(observation_time__range=(timezone.now() - timedelta(days=21), timezone.now())).exists():
-            raise ValidationError('Cow cannot be in heat within 21 days of previous heat observation.')  
-        
-        # Check 8: Ensure that cow is not less than 10 months old
-        if self.cow.get_cow_age() < 300:
-            raise ValidationError('Cow must be at least 10 months old to be in heat.')
-
 
 class Pregnancy(models.Model):
     """
@@ -297,20 +193,23 @@ class Pregnancy(models.Model):
 
 
 class Lactation(models.Model):
-    """A model representing a lactation period of a cow."""
+    """
+    A model representing a lactation period of a cow.
+    """
     class Meta:
-        verbose_name_plural = "Lactation records"
+        verbose_name_plural = "Lactation"
         unique_together = ('start_date', 'cow')
-        
-    LACTATION_STAGES = ((1, "Early"),(2, "Mid"), (3, "Late"),(4, "Dry"))
+        get_latest_by = 'start_date'
+
     start_date = models.DateField()
-    end_date = models.DateField(null=True, blank=True)
-    lactation_stage = models.IntegerField(editable=False,choices=LACTATION_STAGES)
-    cow = models.ForeignKey(Cow, on_delete=models.PROTECT)
-    pregnancy = models.OneToOneField(Pregnancy, on_delete=models.CASCADE)
-    lactation_number = models.PositiveSmallIntegerField(default=1)
-    
+    end_date = models.DateField(null=True, editable=False)
+    cow = models.ForeignKey(Cow, on_delete=models.PROTECT, editable=False)
+    lactation_number = models.PositiveSmallIntegerField(default=1, editable=False)
+    pregnancy = models.OneToOneField(Pregnancy, on_delete=models.CASCADE, editable=False)
+
+
     def days_in_lactation(self):
+        """Returns the days in lactation for this lactation instance."""
         today = datetime.today().date()
         if self.end_date:
             end_date = self.end_date
@@ -318,29 +217,171 @@ class Lactation(models.Model):
             end_date = today
         days = (end_date - self.start_date).days
         return days
-    
+
+    @property
+    def lactation_stage(self):
+        days = self.days_in_lactation()
+        
+        if self.end_date:
+            return "Ended"
+        elif days <= 100:
+            return "Early"
+        elif days <= 200:
+            return "Mid"
+        elif days <= 260:
+            return "Late"
+        else:
+            return "Dry"
+
     @property
     def lactation_duration(self):
         if self.end_date and self.start_date:
-            return (self.end_date - self.start_date).days
+            return f"{(self.end_date - self.start_date).days} days"
         else:
-            return None
-
-    def save(self, *args, **kwargs):
-        days = self.days_in_lactation()
-
-        if days <= 100:
-            self.lactation_stage = 1
-        elif days <= 200:
-            self.lactation_stage = 2
-        elif days <= 300:
-            self.lactation_stage = 3
+            return f"{self.days_in_lactation()} days" 
+           
+    @property
+    def end_date_(self):
+        if self.end_date:
+            return self.end_date.strftime('%Y-%m-%d')
         else:
-            self.lactation_stage = 4
+            return "Ongoing"
+        
+    def __str__(self):
+        return f"Lactation record {self.lactation_number} for {self.cow}"
 
-        super().save(*args, **kwargs)
 
+class Milk(models.Model):
+    """
+    The Milk model is a representation of a cow's milk production. It contains information about the milking time,
+    milking date, the cow that produced the milk, the amount of milk produced, and the lactation stage of the cow at the time of milking.
+    """
+    class Meta:
+        verbose_name_plural = "Milk"
+        constraints = [
+            models.UniqueConstraint(fields=['cow', 'milking_date'], name='unique_milk_record'),
+        ]
+        
+    milking_date = models.DateTimeField(auto_now_add=True)
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE, related_name='milk')
+    amount_in_kgs = models.DecimalField(verbose_name="Amount (kg)", default=0.00 ,max_digits=5,decimal_places=2, 
+                                        validators=[MinValueValidator(0, message="Amount of milk can not be less than 0 Kgs."),
+                                                    MaxValueValidator(35, message="Amount of milk cannot be more than 35.0 Kgs.")])
+    lactation = models.ForeignKey(Lactation, on_delete=models.CASCADE, editable=False, null=True)
+         
+    def __str__(self):
+        return f"Milk record of cow {self.cow.name} on {self.milking_date.strftime('%Y-%m-%d %H:%M:%S')}"
+
+    def clean(self):
+        cow = self.cow
+        latest_lactation = self.cow.lactation_set.last()
+        
+        if latest_lactation is None:
+            raise ValidationError("Cannot add milk entry, cow has no active lactation")
+        
+        elif latest_lactation.lactation_stage == "Dry":
+            raise ValidationError("Cannot add milk entry, Cow has been dried off")
+        
+        elif latest_lactation.lactation_stage not in ["Early", "Mid", "Late"]:
+            raise ValidationError("Cannot add milk entry, cow's active lactation is not in stages Early, Mid, or Late")
+        
+        if cow.availability_status == "D":
+            raise ValidationError("Cannot add milk record for a dead cow.")
+        
+        if cow.availability_status == "S":
+            raise ValidationError("Cannot add milk record for sold cow.")
+        
+        if cow.get_cow_age() < 21*30:
+            raise ValidationError('Cow is less than 21 months old and should not have a milk record')
+
+        if cow.gender != "F":
+            raise ValidationError("This cow is not female and cannot produce milk.")
+
+        if self.amount_in_kgs <= 0:
+            raise ValidationError("Amount in kgs should be greater than 0")
+        
+        # if self.milking_date > timezone.now():
+        #     raise ValidationError("Milking date cannot be in the future.")
+
+
+class WeightRecord(models.Model):
+    class Meta:
+        unique_together = ('cow', 'date')
+
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)
+    weight = models.DecimalField(
+        default = 1,
+        max_digits=6, 
+        decimal_places=2,
+        help_text="Enter weight in Kgs.",
+        validators=[
+            MinValueValidator(1, message="Invalid weight. A cow's minimum weight record can not be less than 1 Kgs."),
+            MaxValueValidator(1500, message="Invalid weight. A cow's maximum weight can not exceed 1500 Kgs.")])
+    date = models.DateField()
     
+    
+    def clean(self):
+        # Check that the date of the weight record is not in the future.
+        if self.date > date.today():
+            raise ValidationError("Weight record date cannot be in the future.")
+        
+        # Check that the cow is alive at the time of the weight measurement.
+        if self.cow.availability_status == 'D':
+            raise ValidationError("Weight cannot be recorded for a dead cow.")
+        
+        # Check that the cow is present at the time of the weight measurement.
+        if self.cow.availability_status == 'S':
+            raise ValidationError("Weight cannot be recorded for a sold cow.") 
+    
+    def __str__(self):
+        return f"{self.cow} - Weight: {self.weight} kgs - Date: {self.date}"
+  
+
+
+   
+class Heat(models.Model):
+    """
+    The Heat model represents a record of a cow being in heat.
+    """
+    class Meta:
+        verbose_name_plural = "Heat records"
+    observation_time = models.DateTimeField()
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE)   
+    
+    def __str__(self):
+        return f"Heat record for cow {self.cow.tag_number} on {self.observation_time}"
+    
+    def clean(self):
+        # Check 1: Ensure that observation time is not in the future
+        if self.observation_time > timezone.now():
+            raise ValidationError('Observation time cannot be in the future.')
+        
+        # Check 2: Ensure that cow is not already pregnant
+        if self.cow.pregnancy_status == 'P':
+            raise ValidationError('Cow is already pregnant.')
+        
+        # Check 3: Ensure that cow is not already in heat
+        if self.cow.heat_set.filter(observation_time__range=(timezone.now() - timedelta(days=2), timezone.now() + timedelta(days=2))).exists():
+            raise ValidationError('Cow is already in heat.')
+        
+        # Check 4: Ensure that cow is alive
+        if self.cow.availability_status == 'D':
+            raise ValidationError('Cow is dead and cannot be in heat.')
+        # Check 5: Ensure that cow is female
+        if self.cow.gender == 'M':
+            raise ValidationError('Heat can only be observed in female cows.')
+        
+        # Check 6: Ensure that cow is not calved in the last 60 days
+        if self.cow.pregnancy_status == 'C' and self.observation_time - self.cow.pregnancy.calving_date < timedelta(days=60):
+            raise ValidationError('Cow cannot be in heat within 60 days after calving.')
+        
+        # Check 7: Ensure that cow is not in heat within 21 days
+        if self.cow.heat_set.filter(observation_time__range=(timezone.now() - timedelta(days=21), timezone.now())).exists():
+            raise ValidationError('Cow cannot be in heat within 21 days of previous heat observation.')  
+        
+        # Check 8: Ensure that cow is not less than 10 months old
+        if self.cow.get_cow_age() < 300:
+            raise ValidationError('Cow must be at least 10 months old to be in heat.')
 
 
 
