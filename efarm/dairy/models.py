b03bfa9c9600747b1675beef6ee7caf4253832b6
Peter Evance <peterevance1@gmail.com>
2023-03-01 21:37:02 +0300

    New models additions

--- efarm/dairy/models.py
+++ efarm/dairy/models.py
@@ -5,7 +5,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
-# Create your models here.
 
 class Breed(models.Model):
     """stores information about breed"""
@@ -13,10 +12,13 @@ class Breed(models.Model):
     BREED_CHOICES = ((FRIESIAN, 'Friesian'),(AYRSHIRE, 'Ayrshire'),(JERSEY, 'Jersey'),(CROSSBREED, 'Crossbreed'),(GUERNSEY, 'Guernsey'),)
     name = models.CharField(max_length=32, choices=BREED_CHOICES,unique=True)
     
+    class Meta:
+        verbose_name = "Breed \U0001F41E"
+        verbose_name_plural = "Breeds \U0001F41E"
+    
     def __str__(self):
         return self.name.capitalize()
-    
-    
+     
 class Cow(models.Model):
     """Stores information about cows"""
     
@@ -33,6 +35,10 @@ class Cow(models.Model):
     availability_status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='A')
     pregnancy_status = models.CharField(max_length=1, choices=PREGNANCY_STATUS, default='N')
     date_of_death = models.DateField(validators=[MaxValueValidator(date.today())],error_messages={'max_value': 'The date of death cannot be in the future!.'}, blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "Cow \U0001F404"
+        verbose_name_plural = "Cows \U0001F404"
     
     @property
     def tag_number(self):  
@@ -92,14 +98,14 @@ class Cow(models.Model):
     def __str__(self):
         return self.name
     
-
 class Pregnancy(models.Model):
     """
     This model represents a pregnancy record for a cow. It tracks the start date, end date, due date, date of calving,
     pregnancy status, sire, pregnancy notes, calving notes, pregnancy scan date, artificial insemination and pregnancy outcome.
     """
     class Meta:
-        verbose_name_plural = "Pregnacy records"
+        verbose_name = "Pregnancies \U0001F930"
+        verbose_name_plural = "Pregnancies \U0001F930"
         
     cow = models.ForeignKey(Cow, on_delete=models.PROTECT, related_name='pregnancies')
     start_date = models.DateField()
@@ -191,13 +197,13 @@ class Pregnancy(models.Model):
         if self.pregnancy_outcome == 'S' and self.pregnancy_status != 'C':
             raise ValidationError("Pregnancy status must be 'Confirmed' if pregnancy outcome is 'Stillborn'.")
 
-
 class Lactation(models.Model):
     """
     A model representing a lactation period of a cow.
     """
     class Meta:
-        verbose_name_plural = "Lactation"
+        verbose_name = "Lactation \U0001F37C"
+        verbose_name_plural = "Lactations \U0001F37C"
         unique_together = ('start_date', 'cow')
         get_latest_by = 'start_date'
 
@@ -250,14 +256,14 @@ class Lactation(models.Model):
     def __str__(self):
         return f"Lactation record {self.lactation_number} for {self.cow}"
 
-
 class Milk(models.Model):
     """
     The Milk model is a representation of a cow's milk production. It contains information about the milking time,
     milking date, the cow that produced the milk, the amount of milk produced, and the lactation stage of the cow at the time of milking.
     """
     class Meta:
-        verbose_name_plural = "Milk"
+        verbose_name = "Milk \U0001F95B"
+        verbose_name_plural = "Milk \U0001F95B"
         constraints = [
             models.UniqueConstraint(fields=['cow', 'milking_date'], name='unique_milk_record'),
         ]
@@ -303,9 +309,10 @@ class Milk(models.Model):
         # if self.milking_date > timezone.now():
         #     raise ValidationError("Milking date cannot be in the future.")
 
-
 class WeightRecord(models.Model):
     class Meta:
+        verbose_name = "Weight \U00002696"
+        verbose_name_plural = "Weight \U00002696"
         unique_together = ('cow', 'date')
 
     cow = models.ForeignKey(Cow, on_delete=models.CASCADE)
@@ -335,10 +342,7 @@ class WeightRecord(models.Model):
     
     def __str__(self):
         return f"{self.cow} - Weight: {self.weight} kgs - Date: {self.date}"
-  
-
-
-   
+    
 class Heat(models.Model):
     """
     The Heat model represents a record of a cow being in heat.
@@ -384,5 +388,262 @@ class Heat(models.Model):
             raise ValidationError('Cow must be at least 10 months old to be in heat.')
 
 
+class Inseminator(models.Model):
+    """
+    The Inseminator model is used to store information about the inseminators that are used in the farm.
+    Inseminators are responsible for artificially inseminating cows in the farm.
+    """ 
+    class Meta:
+        verbose_name = "AI Technician \U0001F3AF"
+        verbose_name_plural = "AI Technicians \U0001F3AF"
+            
+    name = models.CharField(max_length=64)
+    company = models.CharField(max_length=64, blank=True, null=True)
+    license_number = models.CharField(max_length=32, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField()
+    address = models.CharField(max_length=256, blank=True, null=True)
+    notes = models.TextField(blank=True)
 
+    def __str__(self):
+        return f'{self.name} ({self.company})'
+           
+class Semen(models.Model):
+    """
+    This model represents the semen records of a cow. It contains information about the inseminator who produced the semen, the producer of the semen, the semen batch number, the date of production, the date of storage and the expiration date.
+    It also allows for notes to be added to the record.
+    """
+    class Meta:
+        verbose_name = "Semen \U0001F4A5"
+        verbose_name_plural = "Semen \U0001F4A5"
+        
+    inseminator = models.ForeignKey(Inseminator, on_delete=models.PROTECT)
+    producer = models.CharField(max_length=64, choices=(('KALRO', 'Kenya Agricultural and Livestock Research Organization'), 
+                                                        ('KAGRIC', 'Kenya Agricultural and Livestock Research Institute')))
+    semen_batch = models.CharField(max_length=64)
+    date_of_production = models.DateField(validators=[MaxValueValidator(date.today())], error_messages={"max_value":"Invalid date entry, Dates of production must not be in future"})
+    date_of_expiry = models.DateField(validators=[MinValueValidator(date.today())], error_messages={"min_value":"Invalid date entry, Date of expiry must be in future"})
+    notes = models.TextField(blank=True)
     
+    def __str__(self):
+        return f"Semen batch {self.semen_batch} produced by {self.producer} on {self.date_of_production}"
+
+class Insemination(models.Model):
+    """
+    The Insemination model represents a record of an insemination event for a cow.
+    It captures the date of insemination, the cow that was inseminated, the pregnancy that resulted from the insemination (if successful),
+    notes about the insemination, and the inseminator that performed the insemination.
+    """
+    class Meta:
+        verbose_name = "AI Record \U0001F4C6"
+        verbose_name_plural = "AI Records \U0001F4C6"
+        
+    date_of_insemination = models.DateField(validators=[MaxValueValidator(date.today())], error_messages={"max_value":"Invalid date entry, Dates must not be in future"})
+    cow = models.ForeignKey(Cow, on_delete=models.PROTECT)
+    pregnancy = models.ForeignKey(Pregnancy, on_delete=models.PROTECT,editable=False, blank=True, null=True)
+    success = models.BooleanField(default=False)
+    notes = models.TextField(blank=True)  
+    inseminator = models.ForeignKey(Inseminator, on_delete=models.PROTECT, blank=True, null=True)
+    semen = models.ForeignKey(Semen, on_delete=models.PROTECT, blank=True, null=True)
+    
+    @property
+    def days_since_insemination(self):
+        """
+        Calculates the number of days elapsed since the insemination event.
+        """
+        elapsed_time = timezone.now().date() - self.date_of_insemination
+        print(elapsed_time)
+        return f"{elapsed_time.days} days"
+    
+    def __str__(self):
+        return f"Insemination record for cow {self.cow.tag_number} on {self.date_of_insemination}"
+
+    def clean(self):
+        # Check 1: Ensure that the cow is alive
+        if self.cow.availability_status == 'D':
+            raise ValidationError('Cannot inseminate a dead cow.')
+        
+        # Check 2: Ensure that the cow is not already pregnant
+        if self.cow.pregnancy_status == 'P':
+            raise ValidationError('Cannot inseminate a cow that is already pregnant.')
+
+        # Check 3: Ensure that the cow has been in heat within the last 3 days
+        if not Heat.objects.filter(cow=self.cow, observation_time__range=(self.date_of_insemination - timedelta(days=3), self.date_of_insemination + timedelta(days=3))).exists():
+            raise ValidationError('Cow must be in heat at the time of insemination.')
+        
+        # Check 4: Ensure that the cow is not less than 12 months old
+        if self.cow.get_cow_age() < 350:
+            raise ValidationError('Cow must be at least 12 months old to be inseminated.')
+        
+        # # Check 5: Ensure that the cow is not inseminated within 21 days
+        if self.cow.insemination_set.filter(date_of_insemination__range=(timezone.now() - timedelta(days=21), timezone.now())).exists():
+            raise ValidationError('Cow cannot be inseminated within 21 days of a previous insemination.')
+
+class Culling(models.Model):
+    MEDICAL_REASONS = [('disease', 'Disease'), ('illness', 'Illness'), ('injuries', 'Injuries'), ('chronic_health', 'Chronic Health Issues'),]
+    FINANCIAL_REASONS = [('cost_of_care', 'Cost of Care'),('unprofitable', 'Unprofitable'), ('low_market_demand', 'Low Market Demand'),]
+    PRODUCTION_REASONS = [('low_production', 'Low Production'),('poor_quality', 'Poor Quality'), ('inefficient_feed', 'Inefficient Feed Conversion'),]
+    GENETIC_REASONS = [('inherited_diseases', 'Inherited Diseases'), ('inbreeding', 'Inbreeding'), ('unwanted_traits', 'Unwanted Traits'),]
+    ENVIRONMENTAL_REASONS = [('climate_change', 'Climate Change'), ('natural_disasters', 'Natural Disasters'), ('overpopulation', 'Overpopulation'),]
+    LEGAL_REASONS = [('government_regulations', 'Government Regulations'), ('animal_welfare', 'Animal Welfare Standards'), ('environmental_protection', 'Environmental Protection Laws'),]
+    CULLING_REASONS = MEDICAL_REASONS + FINANCIAL_REASONS + PRODUCTION_REASONS + GENETIC_REASONS + ENVIRONMENTAL_REASONS + LEGAL_REASONS
+    
+    cow = models.ForeignKey(Cow, on_delete=models.CASCADE, related_name='cullings')
+    reason = models.CharField(max_length=100, choices=CULLING_REASONS)
+    date = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "Culling \U0001F5E1"
+        verbose_name_plural = "Cullings \U0001F5E1"
+        ordering = ['-date']
+        
+    def __str__(self):
+        return f"Culling of {self.cow.name} on {self.date}"
+        
+class Pathogen(models.Model):
+    class Meta:
+        verbose_name = "Pathogen"
+        verbose_name_plural = "Pathogens"
+    name = models.CharField(max_length=50,choices=(("Virus","Virus"),("Bacteria","Bacteria"),("Fungi","Fungi")), unique=True)
+
+    def __str__(self):
+        return self.name
+
+class DiseaseCategory(models.Model):
+    class Meta:
+        verbose_name = "Disease category"
+        verbose_name_plural = "Disease categories"
+    name = models.CharField(max_length=50, choices=(("Nutrition","Nutrition"),("Infectious","Infectious"),
+                                                    ("Physiological","Physiological"),("Genetic","Genetic")), unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Symptoms(models.Model):
+    """
+    This class defines the model for symptoms. It has several fields such as name, type, description, date observed, severity, duration, location, visual indicator description, and visual indicator image.
+    """
+    class Meta:
+        verbose_name = "Symptom \U0001F912"
+        verbose_name_plural = "Symptoms \U0001F912"
+    
+    symptom_types = (('respiratory','Respiratory'), ('digestive','Digestive'),
+                     ('reproductive','Reproductive'), ('musculoskeletal','Musculoskeletal'), 
+                     ('metabolic','Metabolic'), ('other','Other'))
+    
+    SEVERITY_CHOICES = (('mild', 'Mild'), ('moderate', 'Moderate'), ('severe', 'Severe'),)
+    LOCATION_CHOICES = (('head', 'Head'), ('neck', 'Neck'), ('chest', 'Chest'),
+    ('abdomen', 'Abdomen'), ('back', 'Back'), ('legs', 'Legs'), ('tail', 'Tail'),
+    ('Whole body', 'Whole body'), ('other', 'Other'),)
+    
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=20, choices=symptom_types)
+    description = models.TextField()
+    date_observed = models.DateField(validators=[MaxValueValidator(date.today())],error_messages={'max_value': 'The date of observation cannot be in the future!.'})
+    severity = models.CharField(max_length=20, choices=SEVERITY_CHOICES)
+    location = models.CharField(max_length=20, choices=LOCATION_CHOICES)
+    
+    def __str__(self):
+        return self.name
+    
+class Treatment(models.Model):
+    """
+    The Treatment model represents a treatment given to a cow for a specific disease or diseases.
+    It has fields for the cow, the date of the treatment, the treatment method, the duration of the treatment, notes, and the treatment status.
+    It also has a property method for displaying the associated diseases. The model also includes a clean method that validates the data before saving, including checking for associated diseases, validating the treatment status, cost, duration and ensuring that the treatment date is not in the future.
+    """
+    class Meta:
+        verbose_name = "Treatment \U0001F48E"
+        verbose_name_plural = "Treatments \U0001F48E"
+    
+    TREATMENT_STATUS_CHOICES = (('scheduled', 'Scheduled'),('in progress', 'In Progress'),
+                                ('completed', 'Completed'),('cancelled', 'Cancelled'),('postponed', 'Postponed'),)
+   
+    disease = models.ForeignKey("dairy.Disease", on_delete=models.CASCADE)
+    cow = models.ForeignKey(Cow, on_delete=models.PROTECT)
+    date_of_treatment = models.DateTimeField()
+    treatment_method = models.TextField(max_length=200)
+    duration = models.IntegerField(blank=True, null=True)
+    notes = models.TextField(blank=True)
+    treatment_method = models.TextField(max_length=200)
+    image = models.ImageField(upload_to='treatment_images/', blank=True, null=True)
+    treatment_status = models.CharField(max_length=15, choices=TREATMENT_STATUS_CHOICES, default='scheduled')
+    cost = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    
+    def clean(self):
+        if self.cost and self.cost <= 0:
+            raise ValidationError('The cost should be greater than zero.')
+        
+        if self.duration and self.duration <= 0:
+            raise ValidationError('The duration should be greater than zero.')
+        
+        if self.date_of_treatment > timezone.now():
+            raise ValidationError('Treatment date cannot be in the future.')
+        
+        if self.treatment_status == 'completed' and not self.duration:
+            raise ValidationError('Duration is required when treatment status is completed.')
+        
+        if self.treatment_status == 'cancelled' and not self.notes:
+            raise ValidationError('A note is required when treatment status is cancelled.')
+        
+        if self.treatment_status == 'postponed' and not self.notes:
+            raise ValidationError('A note is required when treatment status is postponed.')
+        
+        if self.cow.availability_status == 'D':
+            raise ValidationError('Treatment cannot be given to a dead cow.')
+        
+        if self.cow.availability_status == 'S':
+            raise ValidationError('Unavailable cow, This cow had been sold')
+
+        
+        if not self.treatment_method:
+            raise ValidationError('Treatment method should not be left blank.')
+
+class Disease(models.Model):
+    """
+    The Disease model represents a disease or condition that has affected a cow or group of cows. 
+    It contains information such as the name of the disease, the causative agent (virus, bacteria, or fungi), the category of the disease (nutritional, infectious, physiological, or genetic), the date of occurrence, the date of recovery, and notes on the disease. 
+    Additionally, it also contains ManyToManyFields for the affected cows and the symptoms associated with the disease. The model has various methods such as affected_cows, last_treatment, and clean method for validations.
+    """
+    name = models.CharField(max_length=50, unique=True)
+    pathogen = models.ForeignKey(Pathogen, on_delete=models.CASCADE, null=True)
+    categories = models.ForeignKey(DiseaseCategory, on_delete=models.PROTECT,related_name="diseases")
+    occurrence_date = models.DateField()
+    is_recovered = models.BooleanField(default=False)
+    recovered_date = models.DateField(blank=True, null=True)
+    notes = models.TextField(blank=True)
+    cows = models.ManyToManyField(Cow, related_name="diseases")
+    date_created = models.DateTimeField(auto_now_add=True)
+    symptoms = models.ManyToManyField(Symptoms, related_name='diseases')
+    treatments = models.ManyToManyField(Treatment, related_name='diseases', blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Disease \U0001F48A"
+        verbose_name_plural = "Diseases \U0001F48A"
+
+    def __str__(self):
+        return "{} ({}) occurred on {}".format(self.name, self.pathogen.name, self.occurrence_date)
+
+    def clean(self):
+        if self.is_recovered and not self.recovered_date:
+            raise ValidationError("Recovered date is required if the disease is marked as recovered.")
+
+        if self.recovered_date and not self.is_recovered:
+            raise ValidationError("Recovered must be set to True if a recovered date is provided.")
+
+        if self.recovered_date and self.occurrence_date > self.recovered_date:
+            raise ValidationError("Recovered date must be after the occurrence date.")
+
+        if self.occurrence_date > datetime.now().date():
+            raise ValidationError("Occurrence date cannot be in the future.")
+
+        if not self.name:
+            raise ValidationError("Disease name is required.")
+
+        if not self.pathogen:
+            raise ValidationError("Pathogen is required.")
+
+        if self.is_recovered and self.treatments.all().count() == 0:
+            raise ValidationError("At least one treatment is required for a recovered disease.")
