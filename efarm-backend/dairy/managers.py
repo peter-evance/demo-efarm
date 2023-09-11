8130da1a0a0d4770fcbd1660008af718807775ec
Peter Evance <peterevance1@gmail.com>
2023-09-11 23:51:11 +0300

    Added `LactationManager`

--- efarm-backend/dairy/managers.py
+++ efarm-backend/dairy/managers.py
@@ -224,3 +224,40 @@ class PregnancyManager(models.Manager):
     def get_stillborn_pregnancies(self):
         return self.filter(pregnancy_outcome=PregnancyOutcomeChoices.STILLBORN)
 
+
+class LactationManager(models.Manager):
+    @staticmethod
+    def days_in_lactation(lactation):
+        """
+        Calculate the number of days in the lactation period.
+        If the lactation has ended, return the difference between the end date and start date.
+        If the lactation is ongoing, return the difference between the current date and start date.
+        """
+        if lactation.end_date:
+            return (lactation.end_date - lactation.start_date).days
+        else:
+            return (todays_date - lactation.start_date).days
+
+    def lactation_stage(self, lactation):
+        """
+        Determine the stage of lactation based on the number of days.
+        """
+        days_in_lactation = self.days_in_lactation(lactation)
+
+        if lactation.end_date:
+            return LactationStageChoices.ENDED
+        elif days_in_lactation <= 100:
+            return LactationStageChoices.EARLY
+        elif days_in_lactation <= 200:
+            return LactationStageChoices.MID
+        elif days_in_lactation <= 275:
+            return LactationStageChoices.LATE
+        else:
+            return LactationStageChoices.DRY
+
+    @staticmethod
+    def lactation_end_date_formatted(lactation):
+        if lactation.end_date:
+            return lactation.end_date.strftime("%Y-%m-%d")
+        else:
+            return "Ongoing"
