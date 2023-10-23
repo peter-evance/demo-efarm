b719e102bc9a927ec8beb4d852ff8aeda1ca305a
Peter Evance <peterevance1@gmail.com>
2023-10-23 07:47:34 +0300

    Minor refactor: Tuple >> List

--- efarm-backend/efarm/urls.py
+++ efarm-backend/efarm/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
