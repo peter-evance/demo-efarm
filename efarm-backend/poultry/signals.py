defaafe09c4a38a87fc61ac4c7b1c3470c31488a
Peter Evance <peterevance1@gmail.com>
2023-10-30 22:16:50 +0300

    POULTRY APP: Added `create_flock_history` signal...

--- efarm-backend/poultry/signals.py
+++ efarm-backend/poultry/signals.py
@@ -1,6 +1,23 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import *
+
+
+@receiver(pre_save, sender=Flock)
+def create_flock_history(sender, instance, **kwargs):
+    if instance.pk is not None:
+        old_instance = Flock.objects.get(pk=instance.pk)
+
+        # Check if 'current_housing_structure' or 'current_rearing_method' has changed
+        if (
+            instance.current_housing_structure != old_instance.current_housing_structure
+            or instance.current_rearing_method != old_instance.current_rearing_method
+        ):
+            FlockHistory.objects.create(
+                flock=instance,
+                rearing_method=instance.current_rearing_method,
+                current_housing_structure=instance.current_housing_structure,
+            )
 
 
 @receiver(post_save, sender=FlockMovement)
