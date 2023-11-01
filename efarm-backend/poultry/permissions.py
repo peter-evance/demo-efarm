4ed8b6562f7df72035e5a94f245502e224380a2c
Peter Evance <peterevance1@gmail.com>
2023-11-01 16:47:15 +0300

    POULTRY APP: More permission classes

--- efarm-backend/poultry/permissions.py
+++ efarm-backend/poultry/permissions.py
@@ -451,3 +451,147 @@ class CanActOnFlockMovement(BasePermission):
                 {"message": "Authentication credentials were not provided."}
             )
         raise PermissionDenied(self.message)
+
+
+class CanAddFlockInspection(BasePermission):
+    """
+    Custom permission class that allows farm staff to add flock inspection records.
+
+    Raises:
+    - `PermissionDenied`: If the user is not a farm staff.
+
+    Usage:
+        Add the permission class to the view or viewset that requires permission to add flock inspection:
+        permission_classes = [CanViewFlockInspection]
+    """
+
+    message = {
+        "message": "Only farm staff have permission to perform this action."
+    }
+
+    def has_permission(self, request, view):
+        """
+        Check if the current user is a farm staff.
+
+        Returns:
+            bool: True if the user is a farm staff, otherwise raises PermissionDenied.
+        """
+        if request.user.is_authenticated and (
+                request.user.is_farm_owner
+                or request.user.is_farm_worker
+                or request.user.is_farm_manager
+                or request.user.is_assistant_farm_manager
+                or request.user.is_farm_worker
+        ):
+            return True
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed(
+                {"message": "Authentication credentials were not provided."}
+            )
+        raise PermissionDenied(self.message)
+
+
+class CanAddViewUpdateFlockInspection(BasePermission):
+    """
+    Custom permission class that allows farm staff to view flock inspection records.
+
+    Raises:
+    - `PermissionDenied`: If the user is not a farm staff.
+
+    Usage:
+        Add the permission class to the view or viewset that requires permission to view flock inspection:
+        permission_classes = [CanViewFlockInspection]
+    """
+
+    message = {
+        "message": "Only farm staff have permission to perform this action."
+    }
+
+    def has_permission(self, request, view):
+        """
+        Check if the current user is a farm staff.
+
+        Returns:
+            bool: True if the user is a farm staff, otherwise raises PermissionDenied.
+        """
+        if request.user.is_authenticated and (
+                request.user.is_farm_owner
+                or request.user.is_farm_worker
+                or request.user.is_farm_manager
+                or request.user.is_assistant_farm_manager
+                or request.user.is_farm_worker
+        ):
+            return True
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed(
+                {"message": "Authentication credentials were not provided."}
+            )
+        raise PermissionDenied(self.message)
+
+
+class CanDeleteFlockInspection(BasePermission):
+    """
+    Custom permission class that allows farm owners and managers to delete flock inspection records.
+
+    Raises:
+    - `PermissionDenied`: If the user is not a farm owner or a farm manager.
+
+    Usage:
+        Add the permission class to the view or viewset that requires permission to delete flock inspection:
+        permission_classes = [CanDeleteFlockInspection]
+    """
+
+    message = {
+        "message": "Only farm owners and managers have permission to perform this action."
+    }
+
+    def has_permission(self, request, view):
+        """
+        Check if the current user is a farm owner or a farm manager.
+
+        Returns:
+            bool: True if the user is a farm owner or a farm manager, otherwise raises PermissionDenied.
+        """
+        if request.user.is_authenticated and (
+                request.user.is_farm_owner or request.user.is_farm_manager
+        ):
+            return True
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed(
+                {"message": "Authentication credentials were not provided."}
+            )
+        raise PermissionDenied(self.message)
+
+
+class CanActOnFlockBreedInformation(BasePermission):
+    """
+        Custom permission class that allows farm owners and managers to act on flock breed information records.
+
+        Raises:
+        - `PermissionDenied`: If the user is not a farm owner or a farm manager.
+
+        Usage:
+            Add the permission class to the view or viewset that requires permission to act on flock breed information:
+            permission_classes = [CanActOnFlockBreedInformation]
+        """
+
+    message = {
+        "message": "Only farm owners and managers have permission to perform this action."
+    }
+
+    def has_permission(self, request, view):
+        """
+        Check if the current user is a farm owner or a farm manager.
+
+        Returns:
+            bool: True if the user is a farm owner or a farm manager, otherwise raises PermissionDenied.
+        """
+        if request.user.is_authenticated and (
+                request.user.is_farm_owner or request.user.is_farm_manager
+        ):
+            return True
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed(
+                {"message": "Authentication credentials were not provided."}
+            )
+        raise PermissionDenied(self.message)
